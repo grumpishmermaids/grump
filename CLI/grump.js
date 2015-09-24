d7a8cdfd3c841679752630442d15c189ce9f963b
Hack Reactor Students <accounts@hackreactor.com>
2015-09-23 21:41:52 -0700

    Now logs output from child process, and accepts input to child process.

--- CLI/grump.js
+++ CLI/grump.js
@@ -10,24 +10,33 @@ var arg = process.argv.pop();
 var fileName = arg + '.sh';
 var filePath = path.join(__dirname ,'lib', fileName);
 
-  if (fs.existsSync(filePath)) { 
-    spawn('sh', [ filePath ]);
-    // do something 
-  } else {
-    http.get("http://grumpy.keitharm.me/api/lib/" + arg, function(res) {
-      console.log("Got response: " + res.statusCode);
-      var file = fs.createWriteStream(filePath);
+if (fs.existsSync(filePath)) {
+  console.log("exists");
+  var child = spawn('sh', [ filePath ], { stdio: [
+    0, // use parents stdin for child
+    'pipe', // pipe child's stdout to parent
+  ] });
 
-      res.pipe(file);
+  child.stdout.on('data', function(data) {
+    console.log(data.toString()); 
+  });
 
-      file.on('finish', function(){
-          file.close(function() {
-            spawn('sh', [ filePath ]);
-          });
-      });
+} else {
+  http.get("http://grumpy.keitharm.me/api/lib/" + arg, function(res) {
+    console.log("Got response: " + res.statusCode);
+    if(res.statusCode === 200){
+        var file = fs.createWriteStream(filePath);
 
+        res.pipe(file);
+
+        file.on('finish', function(){
+            file.close(function() {
+              spawn('sh', [ filePath ]);
+            });
+        });
+      } 
     }).on('error', function(err) {
       console.log("Got error: " + err.message);
     });
+}
     
-  }
---
+++ CLI/lib/helloworld.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "hello world"
+
+echo -n "Enter your name and press [ENTER]: "
+read name
