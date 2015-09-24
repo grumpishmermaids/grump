0d9bf592cd5b978fc834e839782419a50c2e8353
Hack Reactor Students <accounts@hackreactor.com>
2015-09-23 20:41:44 -0700

    gets a bash file and runs it

--- CLI/grump.js
+++ CLI/grump.js
@@ -5,48 +5,29 @@ var http = require('http');
 var spawn = require('child_process').spawn;
 
 //get the argument
-var fileName = process.argv.pop() + '.sh';
+var arg = process.argv.pop();
+
+var fileName = arg + '.sh';
 var filePath = path.join(__dirname ,'lib', fileName);
 
-//check if i have it
-var file = fs.statSync(filePath);
+  if (fs.existsSync(filePath)) { 
+    spawn('sh', [ filePath ]);
+    // do something 
+  } else {
+    http.get("http://grumpy.keitharm.me/api/lib/" + arg, function(res) {
+      console.log("Got response: " + res.statusCode);
+      var file = fs.createWriteStream(filePath);
 
-if(file.isFile()) {
-  spawn('sh', [ filePath ]);
-} else {
-  http.get("http://grumpy.keitharm.me/api/lib/" + arg, function(res) {
-    console.log("Got response: " + res.statusCode);
-    var writeStream = fs.createWriteStream(filePath);
+      res.pipe(file);
 
-    res.pipe(writeStream);
+      file.on('finish', function(){
+          file.close(function() {
+            spawn('sh', [ filePath ]);
+          });
+      });
 
-    writeStream.on('finish', function(){
-        file.close(function() {
-          spawn('sh', [ filePath ]);
-        });
+    }).on('error', function(err) {
+      console.log("Got error: " + err.message);
     });
-
-  }).on('error', function(err) {
-    console.log("Got error: " + err.message);
-  });
-
-}
-
-
-
-
-// var file = fs.stats(path)
-// if(file.isFile())
-
-  // look in lib
-  // --if this is the first time i run it do i save it?
-
-//have we done this already
-
-//make call to api with argument
-
-//return file from api
-
-//run file
-// console.log(arg);
-// console.log(process.env.HOME + '/desktop/grump/cli');
+    
+  }
