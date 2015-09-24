d4949fa805f17fb562d737d1202ca8b14cbfe80e
Keith Armstrong <keith1015@gmail.com>
2015-09-23 20:31:41 -0700

    Added missing index

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var api = require('./routes/api');
+var index = require('./routes/index');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', index);
 app.use('/api', api);
 
 // catch 404 and forward to error handler
