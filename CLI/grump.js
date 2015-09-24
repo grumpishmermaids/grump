8c6cfc0ee5aa39359b63116b090817ecfbded732
mochuelo-kun <mochuelokun@gmail.com>
2015-09-24 15:46:04 -0700

    Refactor: put modular code into utils.js, with more descriptive comments&variable names

--- CLI/grump.js
+++ CLI/grump.js
@@ -1,26 +1,51 @@
 #!/usr/bin/env node
 
-// required packages
-var http = require('http');
-var fs = require('fs');
+// ### !!IMPORTANT README!! ###
+//  The first line of this file that starts with hashbang ("#!") IS NOT A COMMENT. DO NOT DELETE.
+//  It's part of the magic grump a CLI (Command Line Interface) -- see explanation below
+//    ##THE MAGIC##
+//    -PART 1: We set a "bin" option in our package.json that makes a "grump" command (in "/usr/bin") that points to this grump.js file
+//            (note: grump.js is in turn copied to /usr/local/lib/node_modules/grump by "npm install -g").
+//    -PART 2: The hashbang on line 1 tells the shell (where you ran the command) to execute whatever your
+//             local user environment ("/usr/bin/env") thinks the "node" command is on everything that follows.
+//    -PART 3: All words typed in the original command (e.g. "grump make me a sandwich")
+//             get stored in an environment object (array) called "process.argv", which we will now use to get the grump arguments.
+
+// required node packages
 var path = require('path');
+var fs = require('fs');
+var utils = require('./utils.js');  //local
+var log = utils.log; //laziness!
 
-// required files
-var utils = require('./utils.js')
 
-//global variables -- should factor out at some point
-var url = "http://grumpy.keitharm.me/api/lib/"
-var arg = process.argv.pop();
-var fileName = arg + '.sh';
-var filePath = path.join(__dirname ,'lib', fileName);
+console.dir(process.argv);
 
-//run the file
-if(fs.existsSync(filePath)) {
-  utils.log("file exists");
-  utils.runFile(filePath);
+// get arguments to grump command
+var scriptName = process.argv.pop();  //TODO: make this intelligent.
+console.log("Processing command < %s >", scriptName);
 
+var fileName = scriptName + '.sh'; //assumption: we only have bash scripts
+                            //TODO: if allowing node, this has to change (just to key value store?)
+
+var grumpScriptDirectory = path.join(__dirname ,'lib');
+utils.log("Grump script directory is:", grumpScriptDirectory);
+
+//grunt.js lives wherever npm puts modules you install "globally" (i.e. when using "-g" option on npm install)
+// ( --> so __dirname should be something like "/usr/local/lib/node_modules")
+//so our local stored scripts directory is at "[global node_modules directory]/grump/lib" 
+
+//TODO: turn fileName into directory? run script in that directory identified by key/value pair of command name
+var filePath = path.join(grumpScriptDirectory, fileName);
+utils.log("Expected local file path is:", filePath);
+
+
+//if script file exists locally, run it.
+//Otherwise, get it from server, then run it.
+if(fs.existsSync(filePath)) {  //TODO: fix to non-deprecated file check
+  utils.log("Script < %s > found in local storage!", scriptName);
+  utils.runScript(filePath);
 } else {
-  utils.log("file dosen't exist");
-  utils.loadFile(url+arg, filePath, utils.runFile);
+  utils.log("Could not find script < %s > locally.", scriptName);
+  utils.downloadScript(scriptName, filePath, utils.runScript); 
+  //TODO: refactor download to use promise, not callback
 }
-    
--- CLI/utils.js
+++ CLI/utils.js
@@ -1,44 +1,54 @@
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
 var spawn = require('child_process').spawn;
 
-//runs the file at a given path
-var runFile = function (path) {
-  log("running file");
-  var childProcess = spawn('sh', [ path ], 
-      { stdio: [ 0, 'pipe' ] });
+//TODO: better way to handle server URL?
+var serverApiUrl = "http://grumpy.keitharm.me/api/lib/";
+
+//runs the script file at a given path
+var runScript = function (filePath) {  //should this take 
+  log("Running script at path:", filePath);
   
-  childProcess.stdout.on('data', function(data) {
+  //create a child process* & pipe any standard input/output to this parent process
+  //(so console logs, prompts, etc. appear in terminal where grump was run)
+  //(*see Node API for details: https://nodejs.org/api/child_process.html)
+  //NOTE: for now only does BASH. (TODO: commonjs scripts via node?)
+  var childProcess = spawn('sh', [filePath], { stdio: [0, 'pipe'] });
+  childProcess.stdout.on('data', function (data) {
     log(data.toString()); 
   });
 };
 
+
+//TODO: refactor to use promise instead of callback
 //loads and writes file, then calls a callback on the file path
-var loadFile = function(url, path, callback) {
-  log("loading file");
-  http.get(url, function(res) {
-    utils.log("Got response: " + res.statusCode);
+var downloadScript = function (scriptName, localFilePath, callback) {
+  log("Requesting match for grump < %s > from server...", scriptName);
+  var pathOnServer = serverApiUrl + scriptName;
+  http.get(pathOnServer, function (res) {
+    log("Got response: %s -- %s", res.statusCode); //TODO include body.message from server
 
     //only writes file if it got back a good response
-    if(res.statusCode === 200){
-      var file = fs.createWriteStream(path);
-      res.pipe(file);
-      file.on('finish', function(){
-        file.close(function() {
-          callback(filePath);
+    //TODO: better logging?
+    if (res.statusCode === 200) {
+      var newFile = fs.createWriteStream(localFilePath);
+      res.pipe(newFile);
+      newFile.on('finish', function () {
+        newFile.close(function () {
+          callback(localFilePath);
         });
       });
-    } 
-
+    }
   })
-  .on('error', function(err) {
-    utils.log("Got error: " + err.message);
+  .on('error', function (err) {
+    log("Got error: " + err.message);
   });
 };
 
-//shortens console.log so its less offensive to me
-var log = function(arg) {
-  console.log(arg);
-};
+//FORNOW: shortens console.log so its less offensive to me
+var log = console.log; //TODO: in future, log to a logfile?
 
-exports.runFile = runFile;
-exports.loadFile = loadFile;
+exports.runScript = runScript;
+exports.downloadScript = downloadScript;
 exports.log = log;
