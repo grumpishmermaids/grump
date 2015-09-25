0d1f24439663ce54a1b6c6b9cc7de8855f2819ce
Hack Reactor Students <accounts@hackreactor.com>
2015-09-24 20:14:45 -0700

    now serving a browse view

--- server/public/app/browse/browse.js
+++ server/public/app/browse/browse.js
@@ -1,4 +1,17 @@
 angular.module('grump.browse', [])
 
 .controller('BrowseController', function ($scope, Files) {
+  $scope.grumps = [];
+  console.log("Frumps!", $scope.grumps);
+
+  $scope.getGrumps = function(){
+    Files.getGrumps().then(function (results) {
+      console.log("DATA!",results.data);
+      $scope.grumps = results.data;
+      console.log("Frumps!", $scope.grumps);
+    });
+  };
+
+  $scope.getGrumps();
+
 });
